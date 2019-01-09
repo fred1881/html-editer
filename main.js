b6ed579b94a5c229aecefe14b5062b04343bf97a
fred1881 <45804486+fred1881@users.noreply.github.com>
2019-01-08 18:49:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+			function run(){
+				var textContent = document.getElementById('editor-textarea').value;
+				document.getElementById('viewer').srcdoc = textContent;
+			}
+function red() {
+	document.getElementById("txt").style.color = "red";
+}
+function blue() {
+	document.getElementById("txt").style.color = "blue";
+}
